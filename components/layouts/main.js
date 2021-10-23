205365615e1db72786192df3828fb45f2859f95b
Julius Taylor <juliustaylor@protonmail.com>
2021-10-23 13:43:44 +0200

    rename router -> _router because it is not used' |

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { Box, Container } from '@chakra-ui/react'
 
-const Main = ({ children, router }) => {
+const Main = ({ children, _router }) => {
   return (
     <Box as="main" pb={8}>
       <Head>
